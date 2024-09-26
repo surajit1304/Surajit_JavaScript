96e24386182cc812a61175edbe223e9c20c6f20d
surajit1304 <surajit1304@gmail.com>
2024-09-26 11:08:45 -0700

    Calculator Program 9_26

---
+++ calculator.js
@@ -0,0 +1,38 @@
+// simple calculator program
+
+// 1. text that says ,whast you want to perform. eg, addition,subtraction, multiplication, and division
+//     +, -, *, / 
+// 2. take input of first number
+// 3. take input of second number
+// 4. perform the specified operation  ->
+// 5. result output
+
+// start here
+
+// 1. -> take input from user to perform action
+let operation = prompt("Enter operation (+,-,*,/)")
+console.log(operation)
+
+// 2. -> take input of the first number
+let first = parseFloat(prompt("Enter first number"))
+console.log(first)
+
+// 3. -> take input of the second number
+let second = parseFloat(prompt("Enter the second number"))
+console.log(second)
+
+// 4. -> perform the operation 
+if(operation === "+"){
+   let result= first + second;
+   console.log(result)
+}
+if(operation === "-"){
+  //  
+}
+if(operation === "*"){
+  //  
+}
+if(operation === "/"){
+   // 
+}
+
