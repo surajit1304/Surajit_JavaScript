633591e136c25c0bcb1f92033f609643a3f5612a
surajit1304 <surajit1304@gmail.com>
2024-10-09 08:45:17 -0700

    Must do
    Co-authored-by: shahidali2303 <shahidali2303@users.noreply.github.com>

--- must-do-exercise.js
+++ must-do-exercise.js
@@ -1,28 +1,88 @@
-// 1. declare a constant variable with value = 5
+// // // 1. declare a constant variable with value = 5
 const myConstant = 5;
-// 2. declare a let varible with value = "Surajit"
-let myVariable="Surajit"
-// 3. create an array of name cars and put some values
-let carNames =["Tesla","Honda", "Mercedes"];
+// myConstant = 5; // "=" => assignment operator,
+// when we declare a variable with const datatype then we cannot reassign the value
+// console.log(myConstant)
+
+
+
+// // // 2. declare a let varible with value = "Surajit"
+let myVariable="Surajit"; 
+// console.log(gdpmyVariable)
+
+
+
+// // // 3. create an array of name cars and put some values
+let carNames = ["Tesla","Honda", "Mercedes"];
+console.log(carNames);
+
+
+
 
 // 4. create an object of name country and put some key value pairs like capital,  number of states, etc
 let country = {
-    name: "USA"
-    gdp: "27 Trillion "
-    population: "330 million"
+    name: "USA",
+    population: "330 million",
     perCapitaIncome: "60,000 dollars"
 };
-// 5. create a function of name greet and run it
+
+// get values from an object 
+// 1. dot operator - to access values from an object
+console.log(country.name)
+
+// 2. bracket operator - to access value directly and dynamically
+console.log(country['name'])
+
+console.log(country)
+
+
+
+// 5. create a function of name greet and run it, Function is a datattype 
 function greet() {
     console.log(" Hello, how are you");
 }
 greet();
-// 6. create an function with arrow function syntax , name it sum and do sum of two numbers
-const sum = (a,b) => a+b;
-console.log(sum(5,6));
-// 7. create a function of any name and accept an array as parameter and run the program hello = () => {
-  return "Hello World!";
 
-// 8. create a for loop to print 1 to 10 
+
+// 6. create an function with arrow function syntax , name it sum and do sum of two numbers
+const sum = (a,b) => {return a+ b};//a,b are parameters
+console.log(sum(5,10));//5 and 10 are arguments for the parameters 
+
+
+
+// 7. create a function of any name and accept an array as parameter and console.log the array
+// syntax 
+// function name(parameter=array, integer or datatypes){
+//
+// }
+// name()   -> Call the function
+
+function Hello(arr)
+{ 
+    console.log(arr) //Body : display arr
+    
+};
+
+Hello(carNames); //Call the function
+
+
+
+// // 8. create a for loop to print 1 to 10 
+for (let i=1;i<=10;i++)
+    console.log(i)
+
+
 // 9. Iterate over an array= [1,2,3,4,5,6,7,8,9] and log the result
+let myArray=[1,2,3,4,5,6];
+for(let i=0;i<myArray.length;i++){
+    console.log(myArray[i])}
+
 // 10.Iterate over an object which you have created in task 4
+
+// for of  => iterate over array
+// for in  => iterate over object
+
+
+for (let index in carNames) {// For in is being used for array, not just object in this case 
+  console.log(index, carNames[index]);// printing the value like red, black, purple as per the
+}
